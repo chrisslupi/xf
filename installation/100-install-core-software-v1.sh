f5c693f8a018941677922898eb25cd7f892d0bd1
chrisslupi <gudmei@web.de>
2018-01-18 10:44:47 +0100

    Comment :  on Thu Jan 18 10:44:47 CET 2018

--- installation/100-install-core-software-v1.sh
+++ installation/100-install-core-software-v1.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 set -e
 ##################################################################################################################
-# Written to be used on 64 bits computers
-# Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-##################################################################################################################
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
+
 ##################################################################################################################
 
 #software from 'normal' repositories
 sudo pacman -S --noconfirm --needed archey3 baobab bleachbit catfish clementine conky curl
 sudo pacman -S --noconfirm --needed darktable dconf-editor
-sudo pacman -S --noconfirm --needed dmidecode 
+sudo pacman -S --noconfirm --needed dmidecode
 sudo pacman -S --noconfirm --needed evince evolution filezilla firefox
-sudo pacman -S --noconfirm --needed galculator gimp git gksu glances gnome-disk-utility 
-sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool 
+sudo pacman -S --noconfirm --needed galculator gimp git gksu glances gnome-disk-utility
+sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool
 sudo pacman -S --noconfirm --needed gparted gpick grsync
-sudo pacman -S --noconfirm --needed hardinfo hddtemp htop 
+sudo pacman -S --noconfirm --needed hardinfo hddtemp htop
 sudo pacman -S --noconfirm --needed inkscape inxi lm_sensors lsb-release meld mlocate mpv
-sudo pacman -S --noconfirm --needed nemo net-tools notify-osd openshot plank polkit-gnome 
-sudo pacman -S --noconfirm --needed redshift ristretto sane screenfetch scrot shotwell 
-sudo pacman -S --noconfirm --needed simple-scan simplescreenrecorder smplayer sysstat 
+sudo pacman -S --noconfirm --needed nemo net-tools notify-osd openshot plank polkit-gnome
+sudo pacman -S --noconfirm --needed redshift ristretto sane screenfetch scrot shotwell
+sudo pacman -S --noconfirm --needed simple-scan simplescreenrecorder smplayer sysstat
 sudo pacman -S --noconfirm --needed terminator thunar transmission-cli transmission-gtk tumbler
-sudo pacman -S --noconfirm --needed variety vlc vnstat wget unclutter  
+sudo pacman -S --noconfirm --needed variety vlc vnstat wget unclutter
 sudo pacman -S --noconfirm --needed r gcc-fortran libreoffice-fresh atom vim
 sudo pacman -S --noconfirm --needed texlive-lang kile texmaker
-sudo pacman -S --noconfirm --needed owncloud-client
+sudo pacman -S --noconfirm --needed nextcloud
 
 sudo systemctl enable vnstat
 sudo systemctl start vnstat
@@ -44,4 +37,3 @@ sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils  uudeview  a
 echo "################################################################"
 echo "###################    core software installed  ################"
 echo "################################################################"
-
