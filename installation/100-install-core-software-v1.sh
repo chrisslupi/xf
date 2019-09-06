4715557c62893dd660cd0145ba8821d176409864
chrisslupi <gudmei@web.de>
2019-09-06 14:39:34 +0200

    Comment :  on Fri 06 Sep 2019 02:39:34 PM CEST

--- installation/100-install-core-software-v1.sh
+++ installation/100-install-core-software-v1.sh
@@ -5,10 +5,10 @@ set -e
 ##################################################################################################################
 
 #software from 'normal' repositories
-sudo pacman -S --noconfirm --needed vim archey3 baobab bleachbit catfish clementine conky curl
+sudo pacman -S --noconfirm --needed archey3 baobab bleachbit catfish clementine conky curl
 sudo pacman -S --noconfirm --needed darktable dconf-editor
 sudo pacman -S --noconfirm --needed dmidecode
-sudo pacman -S --noconfirm --needed evince evolution filezilla firefox gvim
+sudo pacman -S --noconfirm --needed evince evolution filezilla firefox
 sudo pacman -S --noconfirm --needed galculator gimp git glances gnome-disk-utility
 sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool
 sudo pacman -S --noconfirm --needed gparted gpick grsync
@@ -19,7 +19,7 @@ sudo pacman -S --noconfirm --needed redshift ristretto sane screenfetch scrot sh
 sudo pacman -S --noconfirm --needed simple-scan simplescreenrecorder smplayer sysstat
 sudo pacman -S --noconfirm --needed terminator thunar transmission-cli transmission-gtk tumbler
 sudo pacman -S --noconfirm --needed variety vlc vnstat wget unclutter
-sudo pacman -S --noconfirm --needed r gcc-fortran libreoffice-fresh atom hunspell-en_US hunspell-de
+sudo pacman -S --noconfirm --needed r gcc-fortran libreoffice-fresh atom vim hunspell-en_US hunspell-de
 sudo pacman -S --noconfirm --needed texlive-lang kile texmaker
 sudo pacman -S --noconfirm --needed nextcloud
 
